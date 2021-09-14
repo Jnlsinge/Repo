40403dbea3afbeaead873684afcbefc8a7827b72
J'Neil Bering <jcstyleshop08@gmail.com>
2021-09-14 23:44:43 +0800

    new program has landed

---
+++ CALCULATOR with night mode/calculatorWNightMode.js
@@ -0,0 +1,48 @@
+AdditionButton = document.getElementById("AdditionButton");
+AdditionOperation = function(){
+    input1 = document.getElementById("input1");
+    input2 = document.getElementById("input2");
+
+    sum = parseInt(input1.value) + parseInt(input2.value);
+    result = document.getElementById("result");
+    result.innerText = sum;
+}
+AdditionButton.addEventListener("click", AdditionOperation);
+
+MinusButton = document.getElementById("MinusButton");
+MinusOperation = function(){
+    input1 = document.getElementById("input1");
+    input2 = document.getElementById("input2");
+
+    diff = parseInt(input1.value) - parseInt(input2.value);
+    result = document.getElementById("result");
+    result.innerText = diff;
+}
+MinusButton.addEventListener("click", MinusOperation);
+
+MultiplicationButton = document.getElementById("MultiplicationButton");
+MultiOperation = function(){
+    input1 = document.getElementById("input1");
+    input2 = document.getElementById("input2");
+
+    multi = parseInt(input1.value) * parseInt(input2.value);
+    result = document.getElementById("result");
+    result.innerText = multi;
+}
+MultiplicationButton.addEventListener("click", MultiOperation);
+
+DivisionButton = document.getElementById("DivisionButton");
+DivOperation = function(){
+    input1 = document.getElementById("input1");
+    input2 = document.getElementById("input2");
+
+    div = parseInt(input1.value) / parseInt(input2.value);
+    result = document.getElementById("result");
+    result.innerText = div;
+}
+DivisionButton.addEventListener("click", DivOperation);
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-theme");
+}
